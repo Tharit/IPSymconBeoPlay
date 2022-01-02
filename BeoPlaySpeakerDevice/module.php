720926684c839bc980bc0b0025bd202983514995
Martin Kleinhans <kleinhans.martin@bcg.com>
2022-01-02 19:51:36 +0100

    initial commit

---
+++ BeoPlaySpeakerDevice/module.php
@@ -0,0 +1,155 @@
+<?php
+
+/**
+API to fetch events
+
+https://192.168.1.1/proxy/network/api/s/default/stat/event?start=1637485298&end=1637488857&_limit=100
+
+ */
+require_once(__DIR__ . '/../libs/ModuleUtilities.php');
+require_once(__DIR__ . '/../libs/JSONSocket.php');
+require_once(__DIR__ . '/../libs/BeoPlayAPI.php');
+
+class UnifiController extends IPSModule
+{
+    use ModuleUtilities;
+    use JSONSocketClient;
+    use BeoPlayAPI;
+
+    public function Create()
+    {
+        //Never delete this line!
+        parent::Create();
+
+        $this->RequireParent('{3CFF0FD9-E306-41DB-9B5A-9D06D38576C3}'); // IO Client Socket
+
+        // timers
+        $this->JSCCreate();
+
+        // variables
+        /*
+        $this->RegisterVariableString("Application", "Application");
+        $this->RegisterVariableString("State", "State");
+        $this->RegisterVariableString("Title", "Title");
+        $this->RegisterVariableFloat("Volume", "Volume", "~Intensity.1");
+        $this->EnableAction("Volume");
+        */
+
+        // messages
+        $this->RegisterMessage(0, IPS_KERNELSTARTED);
+        $this->RegisterMessage($this->InstanceID, FM_CONNECT);
+        $this->RegisterMessage($this->InstanceID, FM_DISCONNECT);
+
+        // clear state on startup
+        $this->ResetState();
+
+        // if this is not the initial creation there might already be a parent
+        if($this->UpdateConnection() && $this->HasActiveParent()) {
+            $this->SendDebug('Module Create', 'Already connected', 0);
+            $this->Disconnect();
+        }
+    }
+
+    /**
+     * Configuration changes
+     */
+    public function ApplyChanges()
+    {
+        $parentID = $this->GetConnectionID();
+
+        if (IPS_GetProperty($parentID, 'Open')) {
+            $this->JSCDisconnect(false);
+            //IPS_SetProperty($parentID, 'Open', false);
+            //@IPS_ApplyChanges($parentID);
+        }
+
+        parent::ApplyChanges();
+
+        if (!IPS_GetProperty($parentID, 'Open')) {
+            IPS_SetProperty($parentID, 'Open', true);
+            @IPS_ApplyChanges($parentID);
+        }
+    }
+
+    public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
+    {
+        $this->JSCMessageSink($TimeStamp, $SenderID, $Message, $Data);
+
+        switch ($Message) {
+            case IPS_KERNELSTARTED:
+            case FM_CONNECT:
+                $this->SendDebug('STARTED / CONNECT', 'resetting connection', 0);
+                // if new parent and it is already active: connect immediately
+                if($this->UpdateConnection() && $this->HasActiveParent()) {
+                    $this->ApplyChanges();
+                }
+                break;
+            case FM_DISCONNECT:
+                $this->ResetState();
+                $this->UpdateConnection();
+                break;
+            case IM_CHANGESTATUS:
+                // reset state
+                $this->ResetState();
+
+                $this->SendDebug('CHANGESTATUS', json_encode($Data), 0);
+
+                // if parent became active: connect
+                if ($Data[0] === IS_ACTIVE) {
+                    $this->Connect();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public function ReceiveData($data) {
+        $this->JSCReceiveData($data);
+    }
+
+    protected function JSCOnDisconnect() {
+        return true;
+    }
+ 
+    protected function JSCOnReceiveData($opCode, $data) {
+        $this->SendDebug('Receive data', json_encode($data));
+    }
+
+    public function RequestAction($ident, $value)
+    {
+        if($ident === 'JSC') {
+            $this->JSCRequestAction($value);
+            return;
+        }
+
+        $this->SendDebug('Action', $ident, 0);
+    }
+
+    //------------------------------------------------------------------------------------
+    // external methods
+    //------------------------------------------------------------------------------------
+    
+
+    //------------------------------------------------------------------------------------
+    // module internals
+    //------------------------------------------------------------------------------------
+    private function ResetState() {
+        $this->JSCResetState();
+    }
+
+    private function Connect() {
+        if($this->JSCGetState() != 0) {
+            IPS_LogMessage('JSC', 'Tried to connect while already connected');
+            return;
+        }
+        $parentID = $this->GetConnectionID();
+        $ip = IPS_GetProperty($parentID, 'Host');
+        $path = '/BeoNotify/Notifications';
+        $this->JSCConnect($ip, $path, $cookie);
+    }
+
+    private function Disconnect() {
+        $this->JSCDisconnect(true);
+    }
+}
---
+++ libs/JSONSocket.php
@@ -0,0 +1,129 @@
+<?php
+
+trait JSONSocketClient {
+    protected function JSCCreate() {
+        $this->RegisterMessage(0, IPS_KERNELSHUTDOWN);
+    }
+
+    protected function JSCResetState() {
+        $this->SetReceiveDataFilter('');
+        $this->MUSetBuffer('Data', '');
+        $this->MUSetBuffer('State', 0);
+        $this->MUSetBuffer('PayloadType', 0);
+        $this->MUSetBuffer('PayloadData', '');
+    }
+
+    protected function JSCSetReceiveDataFilter($filter) {
+        $this->MUSetBuffer('JSCReceiveDataFilter', $filter);
+        if($this->MUGetBuffer('State') == 2) {
+            if($filter) {
+                $this->SetReceiveDataFilter($filter);
+            } else {
+                $this->SetReceiveDataFilter('');
+            }
+        }
+    }
+
+    protected function JSCRequestAction($value) {
+    }
+
+    protected function JSCMessageSink($TimeStamp, $SenderID, $Message, $Data) {
+    }
+
+    /**
+     *
+     */
+    protected function JSCConnect($ip, $path, $cookie)
+    {
+        $Header[] = 'GET ' . $path . ' HTTP/1.1';
+        $Header[] = 'Host: ' . $ip;
+        $Header[] = "\r\n";
+        $SendData = implode("\r\n", $Header);
+        //$this->SendDebug('Send Handshake', $SendData, 0);
+
+        $this->MUSetBuffer('State', 1);
+
+        $JSON['DataID'] = '{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}';
+        $JSON['Buffer'] = utf8_encode($SendData);
+        $JsonString = json_encode($JSON);
+        parent::SendDataToParent($JsonString);
+
+        return true;
+    }
+
+    protected function JSCGetState() {
+        return $this->MUGetBuffer('State');
+    } 
+
+    protected function JSCDisconnect($canReconnect = true) {
+        $parentID = $this->GetConnectionID();
+        if (!IPS_GetProperty($parentID, 'Open')) {
+            return;
+        }
+        IPS_SetProperty($parentID, 'Open', false);
+        @IPS_ApplyChanges($parentID);
+
+        if($canReconnect && $this->JSCOnDisconnect()) {
+            IPS_SetProperty($parentID, 'Open', true);
+            @IPS_ApplyChanges($parentID);
+        }
+    }
+
+    protected function JSCReceiveData($data)
+    {
+        // unpack & decode data
+        $data = json_decode($data);
+        $data = utf8_decode($data->Buffer);
+
+        $state = $this->MUGetBuffer('State');
+        $data = $this->MUGetBuffer('Data') . $data;
+
+        if($state === 0) {
+            $this->SendDebug('Error', 'Unexpected data received while connecting', 0);
+        } else if($state === 1) {
+            try {
+                if (strpos($data, "\r\n\r\n") !== false) {
+                    //$this->SendDebug('Handshake response', $data, 0);
+
+                    $this->MUSetBuffer('Data', '');
+                    $this->MUSetBuffer('State', 2);
+
+                    $filter = $this->MUGetBuffer('JSCReceiveDataFilter');
+                    if($filter) {
+                        $this->SetReceiveDataFilter($filter);
+                    }
+                    return;
+                } else {
+                    $this->SendDebug('Incomplete handshake response', $data, 0);
+                    throw new Exception("Incomplete handshake response received");
+                }
+            }  catch (Exception $exc) {
+                $this->SendDebug('Error', $exc->GetMessage(), 0);
+                $this->JSCDisconnect();
+                trigger_error($exc->getMessage(), E_USER_NOTICE);
+                return;
+            }
+        } else if($state === 2) {
+            while(true) {
+                $idx = strpos($data, '\n');
+                if($idx === false) break;
+                $packet = substr($data, 0, $idx);
+                $data = substr($data, $idx+1);
+                try {
+                    $this->JSCOnReceiveData(json_decode($packet, true));
+                } catch(Exception $e) {
+                    trigger_error("Error in websocket data handler: " . $exc->getMessage(), E_USER_WARNING);
+                    $this->SendDebug('Received Data', $data, 0);
+                }
+            }
+        }
+
+        if(strlen($data) > 1024 * 1024) {
+            $this->JSCDisconnect();
+            trigger_error("Maximum frame size exceeded", E_USER_NOTICE);
+            return;
+        }
+
+        $this->MUSetBuffer('Data', $data);
+    }
+}
